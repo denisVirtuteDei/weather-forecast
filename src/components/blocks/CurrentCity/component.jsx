79c9a6aef6c35676f9b08f7a9d645abc5d2bb1bb
zero.daniel443@gmail.com <zero.daniel443@gmail.com>
2021-03-02 11:29:06 +0300

    connect geoapify autocomplete

--- src/components/blocks/CurrentCity/component.jsx
+++ src/components/blocks/CurrentCity/component.jsx
@@ -1,29 +1,62 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { Div } from './style';
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
+import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 
-import { getUserGeolocationRequest } from '../../../actions/geolocation'
+import { getUserGeolocationRequest } from '../../../actions/geolocation';
 
-
-export const CurrentCity = (props) => {
+const CurrentCity = (props) => {
 
     const geolocation = useSelector(state => state.geolocation.userGeolocation);
     const dispatch = useDispatch();
 
+
+
     useEffect(() => {
         if (!geolocation.city)
-            dispatch(getUserGeolocationRequest());
+            dispatch(getUserGeolocationRequest ());
     }, [])
 
+    const onPlaceSelect = value => {
+        //setQuery(value);
+        console.log(value);
+    }
+
+
     return (
-        <Div >
-            <div className='city-div'>
-                {geolocation.city || 'City'}
-            </div>
-            <div className='region-div'>
-                {geolocation.region || 'Region'}
-            </div>
-        </Div>
+        <GeoapifyContext apiKey="4d452340ced347708fa6f45256620b3f">
+            <GeoapifyGeocoderAutocomplete
+                placeholder="Enter address here"
+                type='city'
+                placeSelect={onPlaceSelect}
+                value={geolocation.city}
+                //value={query !== null ? query.properties.formatted : ''}
+            />
+        </GeoapifyContext>
     )
 }
+
+export default CurrentCity;
+
+// const CurrentCity = (props) => {
+
+//     const geolocation = useSelector(state => state.geolocation.userGeolocation);
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         if (!geolocation.city)
+//             dispatch(getUserGeolocationRequest());
+//     }, [])
+
+//     return (
+//         <Div >
+//             <div className='city-div'>
+//                 {geolocation.city || 'City'}
+//             </div>
+//             <div className='region-div'>
+//                 {geolocation.region || 'Region'}
+//             </div>
+//         </Div>
+//     )
+// }

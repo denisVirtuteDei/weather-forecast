89f3d5bff0696a093ea95bd86e8c8b701bef3005
zero.daniel443@gmail.com <zero.daniel443@gmail.com>
2021-03-21 14:04:48 +0300

    change eslint rules and add prettier

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,17 +1,14 @@
-export const emailValidator = email => email.length !== 0
-    // eslint-disable-next-line no-useless-escape
-    ? /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-        .test(email)
-        ? ''
-        : 'Incorrect email'
-    : ''
+export const emailValidator = (email) =>
+	email.length !== 0
+		? /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+				email
+		  )
+			? ''
+			: 'Incorrect email'
+		: ''
 
-export const passwordLengthValidator = password => password &&
-    password.length < 6
-    ? 'Less than 6 symbols'
-    : ''
+export const passwordLengthValidator = (password) =>
+	password && password.length < 6 ? 'Less than 6 symbols' : ''
 
-export const isEqual = (left, right) => left && right &&
-    left.localeCompare(right) !== 0
-    ? 'Passwords aren\'t equal'
-    : ''
+export const isEqual = (left, right) =>
+	left && right && left.localeCompare(right) !== 0 ? "Passwords aren't equal" : ''
